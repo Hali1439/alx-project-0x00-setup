e47974d260fdc26c491385d57a9c45c8c4c10461
Halima Muktar <halimamuk1307@gmail.com>
2025-07-15 01:37:22 +0300

    Create index.tsx

---
+++ alx-project-0x00/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-2xl font-semibold">
+      Welcome to ALX Project 0x00
+    </div>
+  );
+}
