ae3c6c58c78ee9317055cd4160c29a213210a1ab
Halima Muktar <halimamuk1307@gmail.com>
2025-07-15 05:59:30 +0300

    Update landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 space-y-8">
+      <h1 className="text-2xl font-semibold">Landing Page</h1>
+
+      {/* Render multiple cards */}
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 };
